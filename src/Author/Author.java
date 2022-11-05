0483b108c9495be6c57eed11c842dd61ea404b65
eptrubnikova <eptrubnikova@gmail.com>
2022-11-05 19:59:04 +0600

    lesson 13

--- src/Author/Author.java
+++ src/Author/Author.java
@@ -3,26 +3,12 @@ package Author;
 import java.util.Objects;
 
 public class Author {
-    public static String toString;
     private final String name;
     private final String lastName;
 
     public Author(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Author author = (Author) o;
-        return name.equals(author.name) && lastName.equals(author.lastName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, lastName);
     }
 
     public String getName() {
@@ -37,4 +23,16 @@ public class Author {
         return "Фамилия, имя автора: " + this.name + " " + this.lastName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return name.equals(author.name) && lastName.equals(author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName);
+    }
 }
--- src/Book.java
+++ src/Book.java
@@ -4,19 +4,6 @@ import java.util.Objects;
 
 public class Book {
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return yearPublisher == book.yearPublisher && nameBook.equals(book.nameBook) && author.equals(book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(nameBook, author, yearPublisher);
-    }
-
     private final String nameBook;
     private final Author author;
     private int yearPublisher;
@@ -44,6 +31,19 @@ public class Book {
     }
 
     public String toString() {
-        return this.author + " Название книги: " + this.nameBook +" Год издания: " + this.yearPublisher;
+        return author + " Название книги: " + this.nameBook +" Год издания: " + this.yearPublisher;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearPublisher == book.yearPublisher && nameBook.equals(book.nameBook) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameBook, author, yearPublisher);
     }
 }
